978605e9fdd1223b7dc658b178d10b9e0d2c3271
wiperrenko <chistiakov.na@phystech.edu>
2020-11-15 18:06:09 +0300

    Правка силы

--- solar_model.py
+++ solar_model.py
@@ -19,8 +19,8 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += ((G * body.m * obj.m) / (r ** 2)) * (abs(body.x - obj.x) / r)
-        body.Fy += ((G * body.m * obj.m) / (r ** 2)) * (abs(body.y - obj.y) / r)
+        body.Fx += ((G * body.m * obj.m) / (r ** 2)) * ((obj.x - body.x) / r)
+        body.Fy += ((G * body.m * obj.m) / (r ** 2)) * ((obj.y - body.y) / r)
 
 
 def move_space_object(body, dt):
